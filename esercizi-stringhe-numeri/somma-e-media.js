a8f100ace63e8e6f2c77d8dd6e2f41d2a20f2749
Antoniov93 <73341910+Antoniov93@users.noreply.github.com>
2020-10-24 15:36:21 +0200

    Update somma-e-media.js

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -8,3 +8,14 @@
   
   http://www.imparareaprogrammare.it
 */
+var a = 3;
+var b = 7;
+var c = 1;
+var d = 6;
+var e = 15;
+
+var sum = (a+b+c+d+e);
+var average = (sum/5);
+
+console.log("Input: a = " + a + ", b = " + b + ", c = " + c + ", d = " + d + ", e = " + e);
+console.log("Output: Somma = " + sum + ", Media = " + average);
