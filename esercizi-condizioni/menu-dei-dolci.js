e7f23d6b95e7393702e57bfaeb1037b513f5beb3
Antoniov93 <73341910+Antoniov93@users.noreply.github.com>
2020-10-26 23:30:01 +0100

    Update menu-dei-dolci.js

--- esercizi-condizioni/menu-dei-dolci.js
+++ esercizi-condizioni/menu-dei-dolci.js
@@ -21,3 +21,38 @@
 
   http://www.imparareaprogrammare.it
 */
+
+
+var sweet = 3;
+var sw1 = "Tiramisù";
+var sw2 = "Torta della nonna";
+var sw3 = "Cheesecake alla nutella";
+var sw4 = "Macedonia";
+
+console.log(`MENU:`);
+console.log(`1. ${sw1}`);
+console.log(`2. ${sw2}`);
+console.log(`3. ${sw3}`);
+console.log(`4. ${sw4}`);
+
+
+console.log(`Dolce Scelto = n° ${sweet}`);
+
+switch (sweet) {
+  case 1:
+    console.log(`Hai scelto il dolce ${sw1}`);
+    break;
+  case 2:
+    console.log(`Hai scelto il dolce ${sw2}`);
+    break;
+  case 3:
+    console.log(`Hai scelto il dolce ${sw3}`);
+    break;
+  case 4:
+    console.log(`Hai scelto il dolce ${sw4}`);
+    break;
+
+  default:
+    console.log(`Dolce non disponibile!`);
+   
+}
