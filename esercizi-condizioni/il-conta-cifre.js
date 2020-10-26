4d62f694d7e756cad3eb4032d29e17caa3411983
Antoniov93 <73341910+Antoniov93@users.noreply.github.com>
2020-10-26 22:28:50 +0100

    Update il-conta-cifre.js

--- esercizi-condizioni/il-conta-cifre.js
+++ esercizi-condizioni/il-conta-cifre.js
@@ -11,3 +11,28 @@
 
   http://www.imparareaprogrammare.it
 */
+
+var num = 95;
+var max = 9999;
+
+console.log(`Numero = ${num}`);
+
+if (num >= 0 && num < 10) {
+  console.log("Il numero è composto da 1 cifra" );
+
+} else if ( num >= 10 && num < 100) {
+  console.log("Il numero è composto da 2 cifre" );
+
+} else if ( num >= 100 && num < 1000) {
+  console.log("Il numero è composto da 3 cifre" );
+
+} else if ( num >= 1000 && num <= 9999) {
+  console.log("Il numero è composto da 4 cifre" );
+
+} else {
+  console.log("Attenzione! Il numero inserito non è corretto!");
+  console.log(`(Inserire un numero tra 0 e ${max})`)
+}
+
+
+
