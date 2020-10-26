d3537420e6ef4672da37a96b67b6fd1cbaec9700
Antoniov93 <73341910+Antoniov93@users.noreply.github.com>
2020-10-26 23:12:43 +0100

    Update maggiore-e-minore.js

--- esercizi-condizioni/maggiore-e-minore.js
+++ esercizi-condizioni/maggiore-e-minore.js
@@ -10,3 +10,43 @@
 
   http://www.imparareaprogrammare.it
 */
+
+var a = 33;
+var b = 8;
+var c = 125;
+var d = 8;
+var max ; 
+var min ;
+
+console.log(`A = ${a}, B = ${b}, C = ${c}, D = ${d} `);
+
+//Comparazione per Maggiore
+if (a>=b && a>=c && a>=d) {
+  max = a;
+  
+} else if (b>=a && b>=c && b>=d) {
+  max = b;
+  
+} else if (c>=a && c>=b && c>=d) {
+  max = c;
+
+} else if (d>=a && d>=b && d>=c) {
+  max = d;
+}
+
+
+//Comparazione per Minore
+if (a<=b && a<=c && a<=d) {
+  min = a;
+  
+} else if (b<=a && b<=c && b<=d) {
+  min = b;
+  
+} else if (c<=a && c<=b && c<=d) {
+  min = c;
+
+} else if (d<=a && d<=b && d<=c) {
+  min = d; 
+}
+
+console.log(`Numero Maggiore = ${max} - Numero Minore = ${min}`);
