292e26eeeaccb3c0e99c0167ff38c463d259c463
Antoniov93 <73341910+Antoniov93@users.noreply.github.com>
2020-10-26 21:17:17 +0100

    Update chi-la-azzecca.js

--- esercizi-condizioni/chi-la-azzecca.js
+++ esercizi-condizioni/chi-la-azzecca.js
@@ -19,3 +19,65 @@
 
   http://www.imparareaprogrammare.it
 */
+
+var pl1 = 6;
+var pl2 = 10;
+var max = 100;
+var min = 1;
+var winner;
+
+console.log("Giocatore 1 = " + pl1);
+console.log("Giocatore 2 = " + pl2);
+
+
+// Generazione Numero Casuale tra max e min
+var numb = Math.floor ( (Math.random() * (max-min) + min) );
+console.log("Numero Casuale Generato = " + numb);
+
+
+
+switch (numb) {
+
+  case pl1:
+    console.log("Il Vincitore è il Giocatore 1!! COMPLIMENTI!!");
+    break;
+
+  case pl2:
+  
+    console.log("Il Vincitore è il Giocatore 2!! COMPLIMENTI!!");
+    break;
+
+  default:
+  
+      if (numb > pl1 && numb > pl2 && pl2>pl1){
+        console.log("Nessuno dei due ha azzeccato il numero casuale, ma il Giocatore 2 si è avvicinato di più!");
+        
+      } else if (numb > pl1 && numb > pl2 && pl2<pl1) {
+        console.log("Nessuno dei due ha azzeccato il numero casuale, ma il Giocatore 1 si è avvicinato di più!");
+        
+      } else if (numb < pl1 && numb < pl2 && pl2>pl1) {
+        console.log("Nessuno dei due ha azzeccato il numero casuale, ma il Giocatore 1 si è avvicinato di più!");
+
+      } else if (numb < pl1 && numb < pl2 && pl2<pl1) {
+        console.log("Nessuno dei due ha azzeccato il numero casuale, ma il Giocatore 2 si è avvicinato di più!");
+
+      } else if (numb>pl1 && numb<pl2 && (numb-pl1)<(pl2-numb)) {
+        console.log("Nessuno dei due ha azzeccato il numero casuale, ma il Giocatore 1 si è avvicinato di più!");
+
+      } else if (numb>pl1 && numb<pl2 && (numb-pl1)>(pl2-numb)) {
+        console.log("Nessuno dei due ha azzeccato il numero casuale, ma il Giocatore 2 si è avvicinato di più!"); 
+
+      } else if (numb>pl2 && numb<pl1 && (numb-pl2)>(pl1-numb)) {
+        console.log("Nessuno dei due ha azzeccato il numero casuale, ma il Giocatore 1 si è avvicinato di più!");
+
+      } else if (numb>pl2 && numb<pl1 && (numb-pl2)<(pl1-numb)) {
+        console.log("Nessuno dei due ha azzeccato il numero casuale, ma il Giocatore 2 si è avvicinato di più!"); 
+
+      } else if (numb>pl2 && numb<pl1 && (numb-pl2)==(pl1-numb)) {
+        console.log("Nessuno dei due ha azzeccato il numero casuale!");
+
+      } else if (numb>pl1 && numb<pl2 && (numb-pl1)==(pl2-numb)) {
+        console.log("Nessuno dei due ha azzeccato il numero casuale");  
+      }  
+}
+
