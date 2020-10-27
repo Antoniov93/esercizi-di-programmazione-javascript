c516c1d6405076a2c8dfef0c6397d179256bd66c
Antoniov93 <73341910+Antoniov93@users.noreply.github.com>
2020-10-27 02:38:46 +0100

    Update rombo-che-stampa.js

--- esercizi-cicli/rombo-che-stampa.js
+++ esercizi-cicli/rombo-che-stampa.js
@@ -16,3 +16,101 @@
   
   http://www.imparareaprogrammare.it
 */
+
+
+
+
+var x = 5;
+var a;
+
+if (x%2==0 || x > 19) {
+  console.log("Numero non Valido!, inserire un numero dispari compreso tra 1 e 19");
+} else {
+
+
+var i;
+for (i = 1; i < x; i=i+2){
+
+
+
+  switch (i) {
+    case 1:
+    a = "         X         ";    
+    break;
+    case 3:
+    a = "        XXX        ";    
+    break;
+    case 5:
+    a = "       XXXXX       ";    
+    break;
+    case 7:
+    a = "      XXXXXXX      ";    
+    break;
+    case 9:
+    a = "     XXXXXXXXX     ";    
+    break;
+    case 11:
+    a = "    XXXXXXXXXXX    ";    
+    break;
+    case 13:
+    a = "   XXXXXXXXXXXXX   ";    
+    break;
+    case 15:
+    a = "  XXXXXXXXXXXXXXX  ";    
+    break;
+    case 17:
+    a = " XXXXXXXXXXXXXXXXX ";    
+    break;
+    case 19:
+    a = "XXXXXXXXXXXXXXXXXXX";    
+    break;
+
+    default:
+    console.log(" Numero non valido! Inserire un valore tra 1 e 19");
+  }
+
+  console.log(`${a}`);
+}
+
+var r;
+for (r = x; r >= 0 ; r=r-2) {
+
+  switch (r) {
+    case 1:
+    a = "         X         ";    
+    break;
+    case 3:
+    a = "        XXX        ";    
+    break;
+    case 5:
+    a = "       XXXXX       ";    
+    break;
+    case 7:
+    a = "      XXXXXXX      ";    
+    break;
+    case 9:
+    a = "     XXXXXXXXX     ";    
+    break;
+    case 11:
+    a = "    XXXXXXXXXXX    ";    
+    break;
+    case 13:
+    a = "   XXXXXXXXXXXXX   ";    
+    break;
+    case 15:
+    a = "  XXXXXXXXXXXXXXX  ";    
+    break;
+    case 17:
+    a = " XXXXXXXXXXXXXXXXX ";    
+    break;
+    case 19:
+    a = "XXXXXXXXXXXXXXXXXXX";    
+    break;
+
+    default:
+    console.log(" Numero non valido! Inserire un valore tra 1 e 19");
+  }
+
+  console.log(`${a}`);
+}
+}
