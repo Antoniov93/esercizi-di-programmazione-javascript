38d7e7a9ae73a9283e4369bbfd99274dfd2ee292
Antoniov93 <73341910+Antoniov93@users.noreply.github.com>
2020-11-03 00:53:15 +0100

    Update azzecca-e-azzera.js

--- esercizi-array/azzecca-e-azzera.js
+++ esercizi-array/azzecca-e-azzera.js
@@ -12,3 +12,53 @@
 
   http://www.imparareaprogrammare.it
 */
+
+var array1 = [];
+var elnumb = 100;       //numero di elementi da avere nell'array
+var min = 1;
+var max = 50;
+
+for (var i = 0; i < elnumb; i++) {
+
+array1 [i] = Math.floor ( (Math.random() * (max-min) + min) );
+
+}
+
+console.log(`Lista di ${elnumb} Numeri (tra ${min} e ${max}) = `+ array1);
+
+//------------------------------------------------------------------------------ 
+
+var accumulator = 0;                         // Definisco una variabile accumulator che è la somma di tutti i numeri dell' array
+for ( var x = 0; x < array1.length; x++) {
+
+ accumulator = accumulator + array1 [x];
+ 
+}
+
+
+while (accumulator != 0){         // definisco un ciclo da ripetere fino a che la somma di tutti i valori dell'array non sono uguali a zero
+
+    var usernum = prompt ("Inserire un numero tra 1 e 50\n(verranno azzerati tutti i suoi multipli dalla lista)") // chiedo il numero da togliere insieme ai suoi multipli
+    for ( var y = 0; y < array1.length; y++) {
+
+        if (array1 [y] % usernum == 0){
+          array1 [y] = 0;
+        }
+
+    }
+    console.log(`Hai Scelto il numero ${usernum}`);
+    console.log(`Lista Aggiornata = ${array1}`);
+
+    accumulator = 0;              // ricalcolo la somma per poterla confrontare di nuovo all'inizio del prossimo ciclo
+    for ( var z = 0; z < array1.length; z++) {
+
+    
+    accumulator = accumulator + array1 [z];
+     
+    }   
+
+}
+
+if (accumulator == 0) {
+  console.log(`La lista è stata completamente azzerata!`);
+}
